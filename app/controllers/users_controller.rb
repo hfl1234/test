21165acdf9d743180416a4250cd068df6bd34175
moohao <mh@lindo.io>
2019-07-11 17:54:11 +0800

    删除注释

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
-  # Never trust parameters from the scary internet, only allow the white list through.
+
   def user_params
     params.require(:user).permit(:name, :age, :address, :password)
   end
